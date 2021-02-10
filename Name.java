e40a43520055ca3ccdc6b3fe423f8b6a432a762b
Simran2812 <77537402+Simran2812@users.noreply.github.com>
2021-02-09 19:51:19 -0500

    user-defined name entered

---
+++ Name.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+ Public static void main (String[] args)
+   Scanner myObj=newScanner(System.in);
+   System.out.printLn("Enter your name");
+   
+   String username=myObj.nextLine();
+   System.out.printLn("Username is: " + username);
+   }
+   
